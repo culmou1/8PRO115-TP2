6b9b43b17fffe9aa6c1a51d4eec24a0a1a8d3199
gchaville <gillian.chaville@gmail.com>
2015-10-06 11:17:12 -0400

    class club up up !

--- class/club.h
+++ class/club.h
@@ -5,28 +5,155 @@
 #include vector
 #include "person.h"
 #include "palmares.h"
+#include "contract.h"
 
 typedef set::vector <int> Date;
 
+// Classe Club
 class Club {
 
 private : 
-	std::string HistoireDuClub;
-	std::string CouleurDuClub;
-	Date AnneeDeCreation;
-	Joueur Effectif;
-	std::vector <Palmares> unPalmares;
-	std::string VilleDuClub;
-	std::vector <Person> StaffTechnique; 
+	std::string 			histoireDuClub;
+	std::string 			couleurDuClub;
+	Date 					anneeDeCreation;
+	std::vector <Joueur> 	*effectif;
+	std::vector <Palmares> 	*unPalmares;
+	std::string 			villeDuClub;
+	std::vector <Person> 	*staffTechnique;
+	std::vector <Contract> 	*contratsdEngagement;
+	std::vector <Rupture> 	*rupturesDeContrats;
+	Calendrier 				calendrier;
 
 public :
     Club();
 
-    ~Club() = default;
+    ~Club();
     Club(const Club& other) = default;
     Club(Club&& other) = default;
     Club& operator=(const Club& other) = default;
     Club& operator=(Club&& other) = default;
+
+//----------------------------------------------------------------- methods for HistoireDuClub
+	std::string getHistoireDuClub() {
+		return histoireDuClub;
+	}
+
+	void setHistoireDuClub(std::string history) {
+		histoireDuClub = history;
+	}
+
+//----------------------------------------------------------------- methods for CouleurDuClub
+	std::string getCouleurDuClub() {
+		return couleurDuClub;
+	}
+
+	void setCouleurDuClub(std::string color) {
+		couleurDuClub = color;
+	}
+
+//----------------------------------------------------------------- methods for AnneeDeCreation
+	Date getAnneeDeCreation() {
+		return anneeDeCreation;
+	}
+
+	void setAnneeDeCreation (Date year) {
+		anneeDeCreation = year;
+	}
+
+//----------------------------------------------------------------- methods for Effectif
+	void getEffectif(std::vector <Joueur> &players) {
+		for (int i = 0; i < effectif->size(); i++) {
+			players[i] = effectif[i];
+		}
+	}
+
+	void setEffectif(std::vector <Joueur> *players) {
+		effectif->clear();
+		for (int i = 0; i < players->size(); i++) {
+			effectif->push_back(players[i]);
+		}	
+	}
+
+//----------------------------------------------------------------- methods for unPalmares
+	void getUnPalmares(std::vector <Palmares> &trophies) {
+		for (int i = 0; i < unPalmares->size(); i++) {
+			trophies[i] = unPalmares[i];
+		}
+	}
+
+	void setPalmares(std::vector <Palmares> *trophies) {
+		unPalmares->clear();
+		for (int i = 0; i < trophies->size(); i++) {
+			unPalmares->push_back(trophies[i]);
+		}	
+	}
+
+//----------------------------------------------------------------- methods for VilleDuClub
+	std::string getVilleDuClub() {
+		return villeDuClub;
+	}
+
+	void setVilleDuClub(std::string town) {
+		villeDuClub = town;
+	} 
+
+//----------------------------------------------------------------- methods for StaffTechnique
+	void getStaffTechnique(std::vector <Person> &staff) {
+		for (int i = 0; i < staffTechnique->size(); i++) {
+			staff[i] = staffTechnique[i];
+		}
+	}
+
+	void setPalmares(std::vector <Person> *staff) {
+		staffTechnique->clear();
+		for (int i = 0; i < staff->size(); i++) {
+			staffTechnique->push_back(staff[i]);
+		}	
+	}
+
+//----------------------------------------------------------------- methods for ContratsdEngagement
+	void getContratsdEngagement(std::vector <Person> &contrats) {
+		for (int i = 0; i < contratsdEngagement->size(); i++) {
+			contrats[i] = contratsdEngagement[i];
+		}
+	}
+
+	void setContratsdEngagement(std::vector <Person> *contrats) {
+		contratsdEngagement->clear();
+		for (int i = 0; i < contrats->size(); i++) {
+			contratsdEngagement->push_back(contrats[i]);
+		}	
+	}
+
+//----------------------------------------------------------------- methods for RupturesDeContrats
+	void getRupturesDeContrats(std::vector <Person> &ruptures) {
+		for (int i = 0; i < rupturesDeContrats.size(); i++) {
+			ruptures[i] = rupturesDeContrats[i];
+		}
+	}
+
+	void setRupturesDeContrats(std::vector <Person> *ruptures) {
+		rupturesDeContrats->clear();
+		for (int i = 0; i < ruptures->size(); i++) {
+			rupturesDeContrats->push_back(ruptures[i]);
+		}	
+	}
+
+//----------------------------------------------------------------- methods for Calendrier
+	Calendrier getCalendrier() {
+		return calendrier;
+	}
+
+	void setCalendrier(Calendrier schedul) {
+		calendrier = schedul;
+	}
+
+//----------------------------------------------------------------- methods of Club
+    void CreerJoueur();
+    void ModifierJoueur();
+    void SupprimerJoueur();
+    void AfficherJoueurs();
+    void TransfertJoueur();
 
 };
 
---
+++ class/contract.h
@@ -0,0 +1,141 @@
+#ifdef CONTRAT_H
+#define CONTRAT_H
+
+#include "club.h"
+
+class Contract {
+
+private :
+	Joueur 	joueurContractant;
+ 	Club 	clubContractant;
+ 	Club 	clubLibere;
+ 	int 	dureeContrat;
+ 	Date 	datedEntree;
+ 	Date 	dateDuContrat;
+ 	float 	seuilTransfert;
+
+public :
+	Contract();
+
+    ~Contract();
+    Contract(const Contract& other) = default;
+    Contract(Contract&& other) = default;
+    Contract& operator=(const Contract& other) = default;
+    Contract& operator=(Contract&& other) = default;
+
+//----------------------------------------------------------------- methods for joueurContractant
+    Joueur getJoueurContractant() {
+    	return joueurContractant;
+    }
+
+    void setJoueurContractant(Joueur newbie) {
+    	joueurContractant = newbie;
+    }
+
+//----------------------------------------------------------------- methods for clubContractant
+    Club getClubContractant() {
+    	return clubContractant;
+    }
+
+    void setClubContractant(Club new_club) {
+    	clubContractant = new_club;
+    }
+
+//----------------------------------------------------------------- methods for clubLibere
+    Club getClubLibere() {
+    	return clubLibere;
+    }
+
+    void setClubLibere(Club old_club) {
+    	clubLibere = old_club;
+    }
+
+//----------------------------------------------------------------- methods for dureeContrat
+    int getDureeContrat() {
+    	return dureeContrat;
+    }
+
+    void setDureeContrat(int duree) {
+    	dureeContrat = duree;
+    }
+
+//----------------------------------------------------------------- methods for datedEntree
+    Date getDatedEntree() {
+    	return datedEntree;
+    }
+
+    void setDatedEntree(Date entree) {
+    	datedEntree = entree;
+    }
+
+//----------------------------------------------------------------- methods for dateDuContrat
+    Date getDateDuContrat() {
+    	return dateDuContrat;
+    }
+
+    void setDateDuContrat(Date d_day) {
+    	dateDuContrat = d_day;
+    }
+
+//----------------------------------------------------------------- methods of Contract
+    void updateSeuilTransfert();
+};
+
+
+
+class Rupture {
+
+private :
+	Joueur 			joueurRelaxant;
+	Club 			clubContractant;
+	std::string 	raisonsDuDepart;
+	float 			penalite;
+
+public :
+	Rupture();
+
+    ~Rupture();
+    Rupture(const Rupture& other) = default;
+    Rupture(Rupture&& other) = default;
+    Rupture& operator=(const Rupture& other) = default;
+    Rupture& operator=(Rupture&& other) = default;
+
+//----------------------------------------------------------------- methods for joueurRelaxant
+    Joueur getJoueurRelaxant() {
+    	return joueurRelaxant;
+    }
+
+    void setJoueurRelaxant(Joueur newbie) {
+    	joueurRelaxant = newbie;
+    } 
+
+//----------------------------------------------------------------- methods for clubContractant
+    Club getClubContractant() {
+    	return clubContractant;
+    }
+
+    void setClubContractant(Club new_club) {
+    	clubContractant = new_club;
+    }
+//----------------------------------------------------------------- methods for raisonsDuDepart
+    std::string getRaisonsDuDepart() {
+    	return raisonsDuDepart;
+    }
+
+    void setRaisonsDuDepart(std::string raisons) {
+    	raisonsDuDepart = raisons;
+    }
+
+//----------------------------------------------------------------- methods for penalite
+    float getPenalite() {
+    	return penalite;
+    }
+
+    void setPenalite(float cost)  {
+    	penalite = cost;
+    }
+
+};
+
+
+#endif
