62996f3c2ecdcd7bec264ee87e9feea867ff1dd0
culmou1 <daehlinadeauotis@gmail.com>
2015-12-03 16:58:50 -0500

    Ajout des des commentaires dans les fonctions

--- class/resultat.h
+++ class/resultat.h
@@ -1,7 +1,7 @@
 #ifndef RESULTAT_H
 #define RESULTAT_H
 /**
- *  La classe resultat, contient le resultat des but locaux et des but visiteurs 
+ *  La classe resultat, contient le resultat des but locaux et des but visiteurs
  */
 
 /**
@@ -14,25 +14,50 @@ private:
 	int _butsVisiteurs;
 
 public:
+	/**
+	* @params Constructeur par defaut
+	*/
 	Resultat() {}
+	/**
+	* @params Constructeur Avec tout les parametres
+	*/
 	Resultat(int home, int visitor) : _butsLocaux(home), _butsVisiteurs(visitor) {}
+	/**
+	* @params Destructeur par defaut
+	*/
 	~Resultat() {}
+	/**
+	* @params Constructeur de recopie
+	*/
 	Resultat(const Resultat& other) : _butsLocaux(other._butsLocaux), _butsVisiteurs(other._butsVisiteurs) {}
+	/**
+	* @params Operateur d'affectation
+	*/
     Resultat& operator=(Resultat&& other);
 //----------------------------------------------------------------- methods for butsLocaux
+/**
+* @params Retrourne les buts locaux
+*/
 	int getButsLocaux() {
 		return _butsLocaux;
 	}
-
+	/**
+	* @params Modifie les buts locaux
+	*/
 	void setButsLocaux(int goals) {
 		_butsLocaux = goals;
 	}
 
 //----------------------------------------------------------------- methods for butsVisiteurs
+/**
+* @params Retrourne les buts visisteur
+*/
 	int getButsVisiteurs() {
 		return _butsVisiteurs;
 	}
-
+	/**
+	* @params Modifie les buts visisteur
+	*/
 	void setButsVisiteurs(int goals) {
 		_butsVisiteurs = goals;
 	}
--- class/rupture.h
+++ class/rupture.h
@@ -9,7 +9,7 @@ class Club;
  *  Rupture, contient un Joueur qui rompte sont contrat
  *	Le club qui obtient le joueur
  *	Une raison de départ en string
- *	Une somme d'argent 
+ *	Une somme d'argent
  */
 class Rupture {
 
@@ -20,52 +20,87 @@ private:
 	double 			_penalite;
 
 public:
+	/**
+	* @params Constructeur par defaut
+	*/
 	Rupture() {}
+	/**
+	* @params Constructeur Avec tout les parametres
+	*/
 
 	Rupture(Joueur *player, Club *new_club, std::string why, double money) :
 		_joueurRelaxant(player), _clubContractant(new_club), _raisonsDuDepart(why), _penalite(money) {}
 
+	/**
+	* @params Destructeur par defaut
+	*/
 	~Rupture() {}
+
+	/**
+	* @params Constructeur de recopie
+	*/
 
     Rupture(const Rupture& other) :
 		_joueurRelaxant(other._joueurRelaxant), _clubContractant(other._clubContractant), _raisonsDuDepart(other._raisonsDuDepart), _penalite(other._penalite) {}
-
+	/**
+	* @params Operateur d'affectation
+	*/
     Rupture& operator=(Rupture&& other) {
 		_joueurRelaxant=other._joueurRelaxant; _clubContractant=other._clubContractant; _raisonsDuDepart=other._raisonsDuDepart; _penalite=other._penalite;
 		return *this;
 	}
 
 //---------------------------------------------------------------- methods for joueurRelaxant
+	/**
+	* @params Retrourne le Joueur Relaxant
+	*/
     Joueur* getJoueurRelaxant() {
     	return _joueurRelaxant;
     }
-
+	/**
+	* @params Modifie le Joueur Relaxant
+	*/
     void setJoueurRelaxant(Joueur *newbie) {
     	_joueurRelaxant = newbie;
     }
 
 //---------------------------------------------------------------- methods for clubContractant
+	/**
+	* @params Retrourne le club contractant
+	*/
     Club* getClubContractant() {
     	return _clubContractant;
     }
-
+	/**
+	* @params Modifie le club contractant
+	*/
     void setClubContractant(Club *new_club) {
     	_clubContractant = new_club;
     }
 //---------------------------------------------------------------- methods for raisonsDuDepart
+/**
+* @params Retrourne la raison de depart
+*/
     std::string getRaisonsDuDepart() {
     	return _raisonsDuDepart;
     }
-
+	/**
+	* @params Modifie la raison de depart
+	*/
     void setRaisonsDuDepart(std::string raisons) {
     	_raisonsDuDepart = raisons;
     }
 
 //---------------------------------------------------------------- methods for penalite
+/**
+* @params Retrourne la penalite
+*/
     double getPenalite() {
     	return _penalite;
     }
-
+	/**
+	* @params Modifie la penalite
+	*/
     void setPenalite(double cost)  {
     	_penalite = cost;
     }
